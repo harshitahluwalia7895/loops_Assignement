e61f176bec257496912926dc6a7fe4f019ff0dc5
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-29 23:58:25 +0530

    final Commit

---
+++ answer7.py
@@ -0,0 +1,13 @@
+dict={}
+
+num=int(input("Enter no. of elements for adding in  dictionary"))
+
+for j in range(num):
+
+    n=input("Enter name and marks as name:marks of student" +str(j+1)+": ")
+
+    li=n.split(':')
+
+    dict[li[0]]=li[1]
+
+print(dict)
